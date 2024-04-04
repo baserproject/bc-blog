f550fae01ed1107aff1c99ec0c8c6c63db021e22
ryuring <ryuring55@gmail.com>
2024-04-04 11:55:25 +0000

    open 5.1-dev

--- src/Service/BlogPostsService.php
+++ src/Service/BlogPostsService.php
@@ -27,9 +27,11 @@ use Cake\Database\Driver\Postgres;
 use Cake\Database\Driver\Sqlite;
 use Cake\Datasource\ConnectionManager;
 use Cake\Datasource\EntityInterface;
+use Cake\Datasource\QueryInterface;
 use Cake\Http\Exception\NotFoundException;
 use Cake\I18n\FrozenTime;
 use Cake\ORM\Query;
+use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 use Cake\Utility\Hash;
 
@@ -45,6 +47,12 @@ class BlogPostsService implements BlogPostsServiceInterface
      * Trait
      */
     use BcContainerTrait;
+
+    /**
+     * BlogPostsTable
+     * @var BlogPostsTable|Table
+     */
+    public BlogPostsTable|Table $BlogPosts;
 
     /**
      * Constructor
@@ -313,7 +321,7 @@ class BlogPostsService implements BlogPostsServiceInterface
         }
         // タグ名
         if ($params['tag']) {
-            $conditions = $this->createTagCondition($conditions, $params['tag']);
+            $query = $this->createTagCondition($query, $params['tag']);
         }
         // 年月日
         if ($params['year'] || $params['month'] || $params['day']) {
@@ -407,30 +415,23 @@ class BlogPostsService implements BlogPostsServiceInterface
     /**
      * タグ条件を生成する
      *
-     * @param array $conditions
+     * @param Query $query
      * @param mixed $tag タグ（配列可）
-     * @return array
+     * @return QueryInterface
      * @checked
      * @noTodo
      * @unitTest
      */
-    public function createTagCondition($conditions, $tag)
+    public function createTagCondition(Query $query, $tag): QueryInterface
     {
         if (!is_array($tag)) $tag = [$tag];
         foreach($tag as $key => $value) {
             $tag[$key] = rawurldecode($value);
         }
-        $tags = $this->BlogPosts->BlogTags->find()
-            ->where(['BlogTags.name IN' => $tag])
-            ->contain(['BlogPosts'])
-            ->all()->toArray();
-        $postIds = Hash::extract($tags, '{n}.blog_posts.{n}.id');
-        if ($postIds) {
-            $conditions['BlogPosts.id IN'] = $postIds;
-        } else {
-            $conditions['BlogPosts.id IS'] = null;
-        }
-        return $conditions;
+        $query->matching('BlogTags', function($q) use ($tag) {
+            return $q->where(['BlogTags.name IN' => $tag]);
+        });
+        return $query;
     }
 
     /**
